75a911bae804bf3ef3586a9c1d0343b33baa3fbb
dagealuba <1120652730@qq.com>
2019-03-16 01:54:49 +0800

    test for merge baranchs

---
+++ src/main/java/servlet/test_2.java
@@ -0,0 +1,19 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/test_2")
+public class test_2 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
