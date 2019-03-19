e3a1ab91ebee7efae759ba633b2bd83f64353317
zhmayy <2399836806@qq.com>
2019-03-20 07:25:10 +0800

    userDaoImpl

--- src/main/java/daoImpl/UserDaoImpl.java
+++ src/main/java/daoImpl/UserDaoImpl.java
@@ -14,7 +14,7 @@ public class UserDaoImpl extends BaseDao implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        List<User> users = new ArrayList<>();
+        List<User> users = new ArrayList<User>();
 
         String sql = "select * from user";
 
@@ -68,21 +68,71 @@ public class UserDaoImpl extends BaseDao implements UserDao {
 
     @Override
     public List<User> getUsersByName(String userName) {
-        return null;
+        List<User> users = new ArrayList<User>();
+
+        String sql = "select * from user where username = ? ";
+
+        Object[] params = {userName};
+        resultSet = this.ExecuteQuery(sql,params);
+
+        try {
+            while(resultSet.next()){
+                User user = new User();
+
+                user.setUserId(resultSet.getString("userid"));
+                user.setUserName(resultSet.getString("username"));
+                user.setUserPassword(resultSet.getString("userpassword"));
+                user.setUserEmail(resultSet.getString("useremail"));
+                user.setAvatarSrc(resultSet.getString("avatarsrc"));
+
+                users.add(user);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            this.closeResource();
+        }
+
+        return users;
     }
 
     @Override
     public boolean addUser(User user) {
-        return false;
+        String sql = "insert into user value(?,?,?,?,?)";
+
+        Object[] params = {user.getUserId(),user.getUserName(),user.getUserPassword(),user.getUserEmail(),user.getAvatarSrc()};
+
+        int row = this.executeUpdate(sql,params);
+        if(row!=0)
+            return true;
+        else
+            return false;
     }
 
     @Override
     public boolean deleteUser(User user) {
-        return false;
+        String sql = "delete from user where userid = ? " ;
+
+        Object[] params = {user.getUserId()};
+        int row = this.executeUpdate(sql,params);
+        if(row!=0)
+            return true;
+        else
+            return false;
     }
 
     @Override
     public boolean updateUser(User user) {
-        return false;
+        String id = user.getUserId();
+
+        String sql = "update user set username = ?,userpassword = ?.usermail = ?,avatarsrc = ? where userid = ?";
+
+        Object[] params = {user.getUserName(),user.getUserPassword(),user.getUserPassword(),user.getUserEmail(),user.getUserEmail(),user.getAvatarSrc(),user.getUserId()};
+
+        int row = this.executeUpdate(sql,params);
+        if(row!=0)
+            return true;
+        else
+             return false;
     }
 }
