8779363df14ae3916a6dd1b6a0f15d640837ac55
蒙荣珍 <1090644593@qq.com>
2019-03-21 07:56:07 +0800

    注册

--- src/main/java/daoImpl/UserDaoImpl.java
+++ src/main/java/daoImpl/UserDaoImpl.java
@@ -44,26 +44,84 @@ public class UserDaoImpl extends BaseDao implements UserDao {
 
     @Override
     public User getUserById(String userId) {
-        return null;
+        String sql="select * from user where userId=?";
+        Object[] params={userId};
+        resultSet = this.ExecuteQuery(sql, params);
+        User user = null;
+        try{
+            while(resultSet.next()){
+                user=new User();
+                user.setUserId(userId);
+                user.setUserName(resultSet.getString("username"));
+                user.setUserPassword(resultSet.getString("userPassword"));
+                user.setUserEmail(resultSet.getString("useremail"));
+                user.setAvatarSrc(resultSet.getString("avatarsrc"));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            this.closeResource();
+        }
+        return user;
     }
 
     @Override
     public List<User> getUsersByName(String userName) {
-        return null;
+        List<User> users = new ArrayList<User>();
+        String sql="select * from user where userName like ?";
+        userName = "%"+userName+"%";
+        Object[] params={userName};
+        resultSet=this.ExecuteQuery(sql,params);
+        try{
+            while(resultSet.next()){
+                User user=new User();
+                user.setUserId(resultSet.getString("userid"));
+                user.setUserName(resultSet.getString("username"));
+                user.setUserPassword(resultSet.getString("userpassword"));
+                user.setUserEmail(resultSet.getString("useremail"));
+                user.setAvatarSrc(resultSet.getString("avatarsrc"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            this.closeResource();
+        }
+        return users;
     }
 
     @Override
     public boolean addUser(User user) {
-        return false;
+        String sql = "insert into user values(?,?,?,?,?)";
+        String userId=user.getUserId();
+        String userName=user.getUserName();
+        String userPassword=user.getUserPassword();
+        String userEmail=user.getUserEmail();
+        String AvatarSrc=user.getAvatarSrc();
+        System.out.println(userName);
+        Object[] params={userId,userName,userPassword,userEmail,AvatarSrc};
+
+        return this.executeUpdate(sql,params)>0;
     }
 
     @Override
     public boolean deleteUser(User user) {
-        return false;
+        String sql = "delete from user where userId = ?";
+        String userId=user.getUserId();
+        Object[] params={userId};
+        return this.executeUpdate(sql,params)>0;
     }
 
     @Override
     public boolean updateUser(User user) {
-        return false;
+        String sql = "update user set userName = ?,userPassword = ?,userEmail = ?,avatarSrc=? where userId = ?";
+        String userId=user.getUserId();
+        String userName=user.getUserName();
+        String userPassword=user.getUserPassword();
+        String userEmail=user.getUserEmail();
+        String avatarSrc=user.getAvatarSrc();
+        Object[] params={userId,userName,userPassword,userEmail,avatarSrc};
+        return this.executeUpdate(sql,params)>0;
     }
 }
