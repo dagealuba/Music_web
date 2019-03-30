2ba7903bed2e7e8a61b5f02b7fa847cf0c40239c
蒙荣珍 <1090644593@qq.com>
2019-03-30 15:49:41 +0800

    注册

--- src/main/java/daoImpl/SearchDaoImpl.java
+++ src/main/java/daoImpl/SearchDaoImpl.java
@@ -3,28 +3,84 @@ package daoImpl;
 import dao.BaseDao;
 import dao.SearchDao;
 import entity.Album;
+import entity.Music;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class SearchDaoImpl extends BaseDao implements SearchDao {
+
+    private ResultSet resultSet=null;
+
     @Override
     public String SearchSinger(String singer) {
 
-        String sql="select * from Music where signer=?";
-        return null;
+        String sql="select * from Music where signer like ?";
+        Object params[]={singer};
+
+        resultSet=this.ExecuteQuery(sql,params);
+        Music music=new Music();
+        try{
+            while(resultSet.next()){
+                music.setSigner(singer);
+                music.getMusicSrc(resultSet.getString("musicSrc"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            this.closeResource();
+        }
+
+        return music.getMusicSrc();
+
+        //return BaseDao.ExecuteQuery(sql,params);
     }
 
     @Override
     public String SearchMusicName(String musicName) {
 
-        String sql="select * from Music where musicName=?";
-        return null;
+        String sql="select * from Music where musicName like ?";
+        Object params[]={musicName};
+        resultSet=this.ExecuteQuery(sql,params);
+        Music music =new Music();
+        try{
+            while(resultSet.next()){
+                music.setMusicName(musicName);
+                music.getMusicSrc(resultSet.getString("musicSrc"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            this.closeResource();
+        }
+        return music.getMusicSrc();
     }
 
     @Override
     public List<Album> SearchAlbum(String albumName) {
+        List<Album> albums=new ArrayList<>();
+        String sql="select * from Album where albumName like ?";
+        Object[] params={albumName};
+        resultSet=this.ExecuteQuery(sql,params);
 
-        String sql="select * from Album";
-        return null;
+        try{
+            while(resultSet.next()){
+                Album album=new Album();
+                album.setAlbumName(albumName);
+                album.setAlbumId(resultSet.getString("albumId"));
+                album.setSinger(resultSet.getString("singer"));
+                albums.add(album);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            this.closeResource();
+        }
+        return albums;
     }
 }
