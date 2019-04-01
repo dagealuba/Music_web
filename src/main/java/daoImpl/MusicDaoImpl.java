2d4b257e22a29bcb0466995219ccefb8ab6a9b5c
zhmayy <2399836806@qq.com>
2019-04-01 22:44:15 +0800

    test

--- src/main/java/daoImpl/MusicDaoImpl.java
+++ src/main/java/daoImpl/MusicDaoImpl.java
@@ -12,6 +12,11 @@ import java.util.List;
 public class MusicDaoImpl extends BaseDao implements MusicDao {
     private ResultSet resultSet = null;
     @Override
+    /**
+     * 功能描述:获取所有歌曲
+     * @param: []
+     * @return: java.util.List<entity.Music>
+     */
     public List<Music> getAllMusic() {
         List<Music> musics =  new ArrayList<Music>();
 
@@ -28,7 +33,7 @@ public class MusicDaoImpl extends BaseDao implements MusicDao {
                 music.setMusicName(resultSet.getString("albumname"));
                 music.setSigner(resultSet.getString("singer"));
                 music.setLyricSrc(resultSet.getString("lyricsrc"));
-                music.setMusicSrc(resultSet.getString("musicsrc");
+                music.setMusicSrc(resultSet.getString("musicsrc"));
                 music.setPicSrc(resultSet.getString("picsrc"));
                 musics.add(music);
             }
@@ -44,6 +49,11 @@ public class MusicDaoImpl extends BaseDao implements MusicDao {
     }
 
     @Override
+    /**
+     * 功能描述:根据歌曲id获得歌曲
+     * @param: [musicid]
+     * @return: entity.Music
+     */
     public Music getMusicById(String musicid) {
         Music music = new Music();
 
@@ -71,6 +81,11 @@ public class MusicDaoImpl extends BaseDao implements MusicDao {
     }
 
     @Override
+    /**
+     * 功能描述:根据歌曲名获取歌曲
+     * @param: [musicname]
+     * @return: java.util.List<entity.Music>
+     */
     public List<Music> getMusicByName(String musicname) {
         List<Music> musics  = new ArrayList<Music>();
         String sql = "select * from music where musicname = ?";
@@ -101,7 +116,12 @@ public class MusicDaoImpl extends BaseDao implements MusicDao {
     }
 
     @Override
-    public List<Music> getMusicBysinger(String singer) {
+    /**
+     * 功能描述:根据歌手获取歌曲
+     * @param: [singer]
+     * @return: java.util.List<entity.Music>
+     */
+    public List<Music> getMusicBySinger(String singer) {
         List<Music> musics  = new ArrayList<Music>();
         String sql = "select * from music where singer = ?";
         Object[] params = {singer};
@@ -131,6 +151,42 @@ public class MusicDaoImpl extends BaseDao implements MusicDao {
     }
 
     @Override
+    /**
+     * 功能描述:根据歌曲名和歌手获取歌曲
+     * @param: [musicname, singer]
+     * @return: entity.Music
+     */
+    public Music getMusicByNameAndSinger(String musicname, String singer) {
+
+        String sql = "select * from music where musicname=? and singer=?";
+        Object[] params={musicname,singer};
+        Music music = new Music();
+
+        resultSet = this.ExecuteQuery(sql,params);
+        try{
+            while(resultSet.next()){
+                music.setMusicId(resultSet.getString("musicid"));
+                music.setMusicName(resultSet.getString("musicname"));
+                music.setSigner(resultSet.getString("singer"));
+                music.setAlbumId(resultSet.getString("albumid"));
+                music.setLyricSrc(resultSet.getString("lyricsrc"));
+                music.setMusicSrc(resultSet.getString("musicsrc"));
+                music.setPicSrc(resultSet.getString("picsrc"));
+               }
+             }catch (SQLException e){
+                 e.printStackTrace();
+           }finally {
+              this.closeResource();
+        }
+        return music;
+    }
+
+    @Override
+    /**
+     * 功能描述:添加歌曲
+     * @param: [music]
+     * @return: boolean
+     */
     public boolean addMusic(Music music) {
 
         String sql = "insert into music values(?,?,?,?,?,?,?)";
@@ -165,6 +221,11 @@ public class MusicDaoImpl extends BaseDao implements MusicDao {
     }
 
     @Override
+    /**
+     * 功能描述:更新歌曲
+     * @param: [music]
+     * @return: boolean
+     */
     public boolean updateMusic(Music music) {
         String sql = "update music musicname = ?,singer = ?,albumid = ?,lyrisrc = ?,musicsrc = ?,picsrc = ? where musicid = ?";
         Object[] params = {music.getMusicName(),music.getSigner(),music.getAlbumId(),music.getLyricSrc(),music.getMusicSrc(),music.getPicSrc(),music.getMusicId()};
@@ -180,4 +241,6 @@ public class MusicDaoImpl extends BaseDao implements MusicDao {
         }
 
     }
+
+
 }
