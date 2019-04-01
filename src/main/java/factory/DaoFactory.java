8fa617fe9f4ea14531f0a18b4711ac20fbedd79e
zhmayy <2399836806@qq.com>
2019-04-01 22:45:04 +0800

    test

--- src/main/java/factory/DaoFactory.java
+++ src/main/java/factory/DaoFactory.java
@@ -1,9 +1,16 @@
 package factory;
 
+import daoImpl.AlbumDaoImpl;
+import daoImpl.MusicDaoImpl;
 import daoImpl.UserDaoImpl;
 
 public class DaoFactory {
     public static UserDaoImpl getUserDaoImpl(){
         return new UserDaoImpl();
     }
+
+    public static AlbumDaoImpl getAlbumDaoImpl(){return new AlbumDaoImpl();}
+
+    public static MusicDaoImpl getMusicDaoImpl(){return new MusicDaoImpl();}
+
 }
