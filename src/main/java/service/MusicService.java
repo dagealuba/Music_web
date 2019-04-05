57d94386f6da311e7f3ee40ffe77d9583ee4232d
zhmayy <2399836806@qq.com>
2019-04-05 23:34:43 +0800

    test

--- src/main/java/service/MusicService.java
+++ src/main/java/service/MusicService.java
@@ -8,4 +8,31 @@ public interface MusicService {
     public boolean deleteMusic(Music music);
 
     public Music findMusicByNameAndSinger(String musicname,String singer);
+
+    /**
+     * @param music
+     * @return true or false
+     */
+    public boolean addMusic(Music music);
+
+    /**
+     * @param musicName
+     * @return List<Music>
+     */
+    public List<Music> getMusicsByName(String musicName);
+
+    /**
+     * 查找是否已有有同名歌
+     * @param music
+     * @return
+     */
+    public boolean checkMusicName(Music music);
+
+
+    /**
+     * 功能描述:
+     * @param:
+     * @return:
+     */
+    public boolean updateMusic(Music music);
 }
