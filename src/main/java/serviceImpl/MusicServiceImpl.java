73974cbb1abc95626da463fb741ff73029587774
zhmayy <2399836806@qq.com>
2019-04-05 23:35:15 +0800

    test

--- src/main/java/serviceImpl/MusicServiceImpl.java
+++ src/main/java/serviceImpl/MusicServiceImpl.java
@@ -4,6 +4,8 @@ import entity.Music;
 import factory.DaoFactory;
 import service.MusicService;
 
+import java.util.List;
+
 public class MusicServiceImpl implements MusicService {
     @Override
     /**
@@ -24,4 +26,38 @@ public class MusicServiceImpl implements MusicService {
     public Music findMusicByNameAndSinger(String musicname, String singer) {
         return DaoFactory.getMusicDaoImpl().getMusicByNameAndSinger(musicname,singer);
     }
+
+    @Override
+    public boolean addMusic(Music music) {
+        System.out.println(checkMusicName(music));
+        if ( !checkMusicName(music) ){
+            return DaoFactory.getMusicDaoImpl().addMusic(music);
+        }
+
+        return false;
+    }
+
+    @Override
+    public List<Music> getMusicsByName(String musicName) {
+        return DaoFactory.getMusicDaoImpl().getMusicsBySongName(musicName);
+    }
+
+    @Override
+    public boolean checkMusicName(Music music) {
+        List<Music> musicList = getMusicsByName(music.getMusicName());
+
+        if (musicList.size() > 0) {
+            for (Music m : musicList) {
+                if (m.getMusicSrc().equals(music.getMusicSrc())) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean updateMusic(Music music) {
+        return DaoFactory.getMusicDaoImpl().updateMusic();
+    }
+
 }
