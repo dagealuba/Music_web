f8ff6c7ce5240399c4d6b4357b704369b2cecec3
蒙荣珍 <1090644593@qq.com>
2019-04-22 15:11:06 +0800

    注册

--- src/main/java/factory/ServiceFactory.java
+++ src/main/java/factory/ServiceFactory.java
@@ -1,9 +1,13 @@
 package factory;
 
+import serviceImpl.SearchServiceImpl;
 import serviceImpl.UserServiceImpl;
 
 public class ServiceFactory {
     public static UserServiceImpl getUserService(){
         return new UserServiceImpl();
     }
+
+    public static SearchServiceImpl getSearchServiceImpl(){return new SearchServiceImpl();}
+
 }
