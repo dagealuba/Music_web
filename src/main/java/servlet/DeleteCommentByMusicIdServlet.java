0047ded15ca5ccfdafd8a56412832ba717b15b04
蒙荣珍 <1090644593@qq.com>
2019-05-12 13:00:54 +0800

    注册

---
+++ src/main/java/servlet/DeleteCommentByMusicIdServlet.java
@@ -0,0 +1,41 @@
+package servlet;
+
+import entity.Comment;
+import factory.ServiceFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/DeleteCommentByMusicIdServlet")
+public class DeleteCommentByMusicIdServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String commentId=request.getParameter("commentId");
+        String musicId=request.getParameter("musicId");
+        String comment=request.getParameter("comment");//评论的内容
+
+        Comment comments = new Comment();
+        comments.setCommentId(commentId);
+        comments.setMusicId(musicId);
+        comments.setComment(comment);
+
+        PrintWriter out = response.getWriter();
+        if (ServiceFactory.getCommentServiceImpl().DeleteCommentByMusicId(musicId)){
+            out.write("true");
+        }
+        else {
+            out.write("false");
+        }
+        out.flush();
+        out.close();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
