8bb5a4154d64e0831e01ca635a61f900d4081dfd
蒙荣珍 <1090644593@qq.com>
2019-05-12 12:56:25 +0800

    注册

--- src/main/java/serviceImpl/CommentServiceImpl.java
+++ src/main/java/serviceImpl/CommentServiceImpl.java
@@ -24,7 +24,7 @@ public class CommentServiceImpl implements CommentService {
     }
 
     @Override
-    public boolean InserComment(Comment comments) {
+    public boolean newComment(Comment comments) {
         return DaoFactory.getCommentDaoImpl().InsertComment(comments);
     }
 
