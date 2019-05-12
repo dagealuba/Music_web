1b98798cc3a97bff4bf42c113932b56ddeacfe0b
蒙荣珍 <1090644593@qq.com>
2019-05-12 12:55:44 +0800

    注册

--- src/main/java/service/CommentService.java
+++ src/main/java/service/CommentService.java
@@ -5,18 +5,45 @@ import entity.Comment;
 import java.util.List;
 
 public interface CommentService {
-    //查看所有的评论
+    /**
+     * 查看评论
+     * @return List<Comment>
+     */
     public List<Comment> AllComment();
-    //通过音乐id 查询评论
+
+    /**
+     * 通过musicId查看评论
+     * @return List<Comment>
+     */
     public List<Comment> getCommentByMusicId(String musicId);
-    //通过评论的id查询评论
+
+    /**
+     * 通过commentId查看评论
+     * @return Comment
+     */
     public Comment getCommentByCommentId(String commentId);
-    //插入评论
-    public boolean InserComment(Comment comments);
-    //通过音乐Id删除评论
+
+    /**
+     * 评论歌曲
+     * @return boolean
+     */
+    public boolean newComment(Comment comments);
+
+    /**
+     * 通过musicId删除评论
+     * @return boolean
+     */
     public boolean DeleteCommentByMusicId(String musicId);
-    //通过评论Id删除评论
+
+    /**
+     * 通过commentId删除评论
+     * @return boolean
+     */
     public boolean DeleteCommentByCommentId(String commentId);
 
+    /**
+     * 更新
+     * @return boolean
+     */
     public boolean updateComment(Comment comments);
 }
