f0009e0adddc5f20adbd19afe3d7e8338bb06eb2
蒙荣珍 <1090644593@qq.com>
2019-05-12 11:50:51 +0800

    注册

--- src/main/java/factory/DaoFactory.java
+++ src/main/java/factory/DaoFactory.java
@@ -1,9 +1,16 @@
 package factory;
 
+import daoImpl.CommentDaoImpl;
+import daoImpl.SearchDaoImpl;
 import daoImpl.UserDaoImpl;
 
 public class DaoFactory {
     public static UserDaoImpl getUserDaoImpl(){
         return new UserDaoImpl();
     }
+
+    public static SearchDaoImpl getSearchDaoImpl(){return new SearchDaoImpl();}
+
+    public static CommentDaoImpl getCommentDaoImpl(){return new CommentDaoImpl();}
+
 }
