0f24175750554ff75066d6887916018b4762a653
蒙荣珍 <1090644593@qq.com>
2019-05-12 11:52:03 +0800

    注册

---
+++ src/main/java/serviceImpl/CommentServiceImpl.java
@@ -0,0 +1,45 @@
+package serviceImpl;
+
+import entity.Comment;
+import factory.DaoFactory;
+import service.CommentService;
+
+import java.util.List;
+
+public class CommentServiceImpl implements CommentService {
+
+    @Override
+    public List<Comment> AllComment() {
+        return DaoFactory.getCommentDaoImpl().getAllComment();
+    }
+
+    @Override
+    public List<Comment> getCommentByMusicId(String musicId) {
+        return DaoFactory.getCommentDaoImpl().CommentByMusicId(musicId);
+    }
+
+    @Override
+    public Comment getCommentByCommentId(String commentId) {
+        return DaoFactory.getCommentDaoImpl().CommentByCommentId(commentId);
+    }
+
+    @Override
+    public boolean InserComment(Comment comments) {
+        return DaoFactory.getCommentDaoImpl().InsertComment(comments);
+    }
+
+    @Override
+    public boolean DeleteCommentByMusicId(String musicId) {
+        return DaoFactory.getCommentDaoImpl().DeleteCommentByMusicId(musicId);
+    }
+
+    @Override
+    public boolean DeleteCommentByCommentId(String commentId) {
+        return DaoFactory.getCommentDaoImpl().DeleteCommentByCommentId(commentId);
+    }
+
+    @Override
+    public boolean updateComment(Comment comments) {
+        return DaoFactory.getCommentDaoImpl().updateComment(comments);
+    }
+}
