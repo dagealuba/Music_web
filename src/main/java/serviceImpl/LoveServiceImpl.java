ed61ab5c3c46bece309b7b3cff77a1ecef7aa751
zhmayy <2399836806@qq.com>
2019-05-13 17:46:37 +0800

    test

--- src/main/java/serviceImpl/LoveServiceImpl.java
+++ src/main/java/serviceImpl/LoveServiceImpl.java
@@ -14,25 +14,8 @@ public class LoveServiceImpl implements LoveService {
 
     @Override
     public boolean addlove(Love love) {
-        if(!checkLoveName(love))
-        {
             return DaoFactory.getLoveDaoImpl().addLove(love);
-        }
-        return false;
-    }
 
-    @Override
-    public boolean checkLoveName(Love love) {
-        List<Love> loves = findLovesByUserId(love.getUserId());
-
-        if(loves.size()>0)
-        {
-            for(Love l:loves){
-                if(l.getLoveName().equals(love.getLoveName()))
-                    return true;
-            }
-        }
-        return false;
     }
 
 
