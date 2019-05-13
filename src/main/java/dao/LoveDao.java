e9398c26f15edf1de343ca97d13247188018f0f0
zhmayy <2399836806@qq.com>
2019-05-13 17:20:32 +0800

    test

--- src/main/java/dao/LoveDao.java
+++ src/main/java/dao/LoveDao.java
@@ -1,12 +1,21 @@
 package dao;
 
 import entity.Love;
+import entity.Music;
 
 import java.util.List;
 
 public interface LoveDao {
-    public List<Love> getAllLove();
+    public List<Love> getAllLove(String userId);
 
     public Love getLoveByLoveId(String loveId);
+
+    public boolean addLove(Love love);
+
+    public boolean updateLove(Love love);
+
+    public boolean deleteLove(Love love);
+
+
 
 }
