4b3c4cd8b21f6219610c5b10b8c6b89a0c5309bb
zhmayy <2399836806@qq.com>
2019-05-13 17:22:28 +0800

    test

---
+++ src/main/java/service/LoveService.java
@@ -0,0 +1,14 @@
+package service;
+
+import entity.Love;
+
+import java.util.List;
+
+public interface LoveService {
+
+    public List<Love> findLovesByUserId(String userid);
+
+    public boolean addlove(Love love);
+
+    public boolean checkLoveName(Love love);
+}
